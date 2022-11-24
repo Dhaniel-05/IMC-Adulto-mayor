00f435dbccd48e2d4c1f5eb86644a208fe14f00d
Daniel Martínez <dhanymarth@gmail.com>
2022-11-24 00:21:46 -0500

    First Commit

---
+++ IMC/IMC.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace IMC
+{
+    partial class IMC
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IMC));
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.TxtPeso = new System.Windows.Forms.TextBox();
+            this.Lbl = new System.Windows.Forms.Label();
+            this.LblPeso = new System.Windows.Forms.Label();
+            this.TxtEstatura = new System.Windows.Forms.TextBox();
+            this.TxtIMC = new System.Windows.Forms.TextBox();
+            this.LblIMC = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(12, 183);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.BtnCalcular.TabIndex = 3;
+            this.BtnCalcular.Text = "Calcular IMC";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(12, 222);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 4;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(12, 262);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(75, 23);
+            this.BtnSalir.TabIndex = 5;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // TxtPeso
+            // 
+            this.TxtPeso.Location = new System.Drawing.Point(12, 32);
+            this.TxtPeso.Name = "TxtPeso";
+            this.TxtPeso.Size = new System.Drawing.Size(42, 20);
+            this.TxtPeso.TabIndex = 1;
+            this.TxtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ValidarPeso);
+            // 
+            // Lbl
+            // 
+            this.Lbl.AutoSize = true;
+            this.Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Lbl.Location = new System.Drawing.Point(9, 16);
+            this.Lbl.Name = "Lbl";
+            this.Lbl.Size = new System.Drawing.Size(92, 13);
+            this.Lbl.TabIndex = 4;
+            this.Lbl.Text = "Digite su peso:";
+            // 
+            // LblPeso
+            // 
+            this.LblPeso.AutoSize = true;
+            this.LblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPeso.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblPeso.Location = new System.Drawing.Point(9, 71);
+            this.LblPeso.Name = "LblPeso";
+            this.LblPeso.Size = new System.Drawing.Size(149, 13);
+            this.LblPeso.TabIndex = 6;
+            this.LblPeso.Text = "Digite su estatura en cm:";
+            // 
+            // TxtEstatura
+            // 
+            this.TxtEstatura.Location = new System.Drawing.Point(12, 87);
+            this.TxtEstatura.Name = "TxtEstatura";
+            this.TxtEstatura.Size = new System.Drawing.Size(42, 20);
+            this.TxtEstatura.TabIndex = 2;
+            this.TxtEstatura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ValidarEstatura);
+            // 
+            // TxtIMC
+            // 
+            this.TxtIMC.Enabled = false;
+            this.TxtIMC.Location = new System.Drawing.Point(12, 143);
+            this.TxtIMC.Name = "TxtIMC";
+            this.TxtIMC.Size = new System.Drawing.Size(75, 20);
+            this.TxtIMC.TabIndex = 7;
+            // 
+            // LblIMC
+            // 
+            this.LblIMC.AutoSize = true;
+            this.LblIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblIMC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblIMC.Location = new System.Drawing.Point(12, 127);
+            this.LblIMC.Name = "LblIMC";
+            this.LblIMC.Size = new System.Drawing.Size(69, 13);
+            this.LblIMC.TabIndex = 8;
+            this.LblIMC.Text = "Su IMC es:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(407, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 300);
+            this.panel1.TabIndex = 9;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(249, 129);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // IMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(659, 300);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LblIMC);
+            this.Controls.Add(this.TxtIMC);
+            this.Controls.Add(this.LblPeso);
+            this.Controls.Add(this.TxtEstatura);
+            this.Controls.Add(this.Lbl);
+            this.Controls.Add(this.TxtPeso);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "IMC";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMC Adulto Mayor";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.TextBox TxtPeso;
+        private System.Windows.Forms.Label Lbl;
+        private System.Windows.Forms.Label LblPeso;
+        private System.Windows.Forms.TextBox TxtEstatura;
+        private System.Windows.Forms.TextBox TxtIMC;
+        private System.Windows.Forms.Label LblIMC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ IMC/IMC.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class IMC : Form
+    {
+        
+        public IMC()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            //Para el cálculo se debe iniciar con comas.
+            //Para calular es IMC, Vamos a 
+            //colocar unos condicionales para que se vean en una ventana emergente
+            //y que le muestre al usuario la clasificación en la que se encuentra
+            //según su IMC.
+            if (String.IsNullOrEmpty(TxtPeso.Text))
+            {
+                MessageBox.Show("Digite su peso", "Notificación");
+                this.TxtPeso.Focus();
+            }
+            else if (String.IsNullOrEmpty(TxtEstatura.Text))
+            {
+                MessageBox.Show("Digite su estatura", "Notificación");
+                this.TxtEstatura.Focus();
+            }
+            try
+            {
+
+                double Peso;
+                Peso = double.Parse(this.TxtPeso.Text);
+                double Estatura;
+                Estatura = double.Parse(this.TxtEstatura.Text);
+                double IMC;
+                IMC = Peso / (Math.Pow(Estatura, 2) / 10000);
+
+                this.TxtIMC.Text = IMC.ToString();
+
+                if (IMC < 18.5)
+                {
+                    MessageBox.Show("Usted tiene Desnutrición", "Notificación");
+                }
+                else if (IMC >= 18.5 && IMC < 22)
+                {
+                    MessageBox.Show("Usted tiene Peso Insuficiente", "Notificación");
+                }
+                else if (IMC >= 22 && IMC < 27)
+                {
+                    MessageBox.Show("Usted tiene Peso Normal", "Notificación");
+                }
+                else if (IMC >= 27 && IMC < 30)
+                {
+                    MessageBox.Show("Usted tiene Sobrepeso", "Notificación");
+                }
+                else
+                {
+                    MessageBox.Show("Usted tiene Obesidad", "Notificación");
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.TxtPeso.Text = String.Empty;
+            this.TxtEstatura.Text = String.Empty;
+            this.TxtIMC.Text = String.Empty;
+        }
+
+
+
+        private void ValidarPeso(object sender, KeyPressEventArgs e)
+        {
+            //Para que el usuario no digite ni punto, ni letras, ni símbolos, se utiliza este código de validación.
+
+            if (e.KeyChar == '.' || Char.IsLetter(e.KeyChar) || Char.IsSymbol(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void ValidarEstatura(object sender, KeyPressEventArgs e)
+        {
+            //Para que el usuario no digite ni punto, ni letras, ni símbolos, se utiliza este código de validación.
+            
+            //if (e.KeyChar == '.' || Char.IsLetter(e.KeyChar) || Char.IsSymbol(e.KeyChar))
+            //{
+            //    e.Handled = true;
+            //}
+
+            e.Handled = e.KeyChar != (char)Keys.Back && !char.IsDigit(e.KeyChar);
+        }
+    }
+}
